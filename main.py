63eb0a1d5548ebe3756b5daca9de765a8ef92260
bharatchandra-dev <bharatchandrasahu13@gmail.com>
2023-05-25 10:23:49 +0530

    Update code with IP filtering

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ import uvicorn
 # run two instances of the app on different ports to simulate a load balancer setup with two servers behind it
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8008, reload=True) 
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8080, reload=True) 
     # uvicorn.run("insurance_server.app:app", host="0.0.0.0", port=8801, reload=True)
